f58f8c102383a3579432d45381741438786a8cea
t29mato <tomoya.matou@gmail.com>
2025-05-21 15:13:35 +0900

    fix: to deploy on streamlit cloud

--- pages/battery.py
+++ pages/battery.py
@@ -2,7 +2,6 @@ import streamlit as st
 import presentation.material_page as material_page
 
 def main():
-    st.title("Battery Material")
     material_page.main("battery")
 
 if __name__ == "__main__":
--- pages/thermoelectric.py
+++ pages/thermoelectric.py
@@ -2,7 +2,6 @@ import streamlit as st
 import presentation.material_page as material_page
 
 def main():
-    st.title("Thermoelectric Material")
     material_page.main("thermoelectric")
 
 if __name__ == "__main__":
